35825345402edc75a9b276c03693faa7a3324a00
Wade Qiu <Wade_Qiu@epam.com>
2025-03-06 10:51:44 +0800

    init project

--- demo-aks/src/main/java/com/wade/demo_aks/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.wade.demo_aks.repository;
-
-import com.wade.demo_aks.entity.OrderTest;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface OrderRepository extends JpaRepository<OrderTest, Long> {
-
-}
--- demo-aks/src/main/java/com/wade/demo_aks/service/OrderService.java
+++ demo-aks/src/main/java/com/wade/demo_aks/service/OrderService.java
@@ -1,17 +1,15 @@
 package com.wade.demo_aks.service;
 
 import com.wade.demo_aks.entity.OrderTest;
-import com.wade.demo_aks.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class OrderService {
-    @Autowired
-    private OrderRepository orderRepository;
     public List<OrderTest> findAllOrders() {
-        return orderRepository.findAll();
+        return Arrays.asList(OrderTest.builder().id(12L).name("Order1").build());
     }
 }
